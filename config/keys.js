d5ba7093ddfd17801dc81d6391956b61697f75f8
Jerry Trujillo <trujillojerry70@yahoo.com>
2020-04-16 13:11:56 -0400

    set up login jsonwebtoken

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI:"mongodb+srv://jerry:belen2016@cluster0-cc3x5.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://jerry:belen2016@cluster0-cc3x5.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: 'secret'
 
 };
