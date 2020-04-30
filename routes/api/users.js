223f41816d905790c87a225de203ce0aa680a17d
Jerry Trujillo <trujillojerry70@yahoo.com>
2020-04-29 21:18:10 -0400

    Add Validation for name

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,11 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
+
+
 // Load User model
 const User = require('../../models/User');
 
@@ -20,6 +25,13 @@ router.get('/test', (req, res) => res.json({msg: "Users Works"}));
 // @access  Public
 
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    //Check Validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({email: req.body.email})
         .then(user => {
             if(user){
